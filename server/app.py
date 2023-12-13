e64e14f375cf6a8c7f12fafd1ea96039c2cf5f57
Romil <romil.m.kothari@gmail.com>
2023-12-13 15:25:33 -0500

    tests completed

--- server/app.py
+++ server/app.py
@@ -23,8 +23,20 @@ def count(parameter):
         number += f'{x}' + "\n"
     return number
     
-# @app.route('/math/<int:num1><operation><int:num2>')
-# def math(num1, operation, num2):
-#     print(isinstance(num1, int))
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    
+    if operation == "+":
+        number = num1 + num2
+    elif operation == "-":
+        number = num1 - num2
+    elif operation == "*":
+        number = num1 * num2
+    elif operation == "div":
+        number = num1 / num2
+    elif operation == "%":
+        number = num1 % num2
+
+    return f'{number}'
 
     
